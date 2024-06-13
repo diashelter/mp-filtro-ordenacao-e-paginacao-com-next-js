2dcc5100c01d1c0b5d38fa17b8fd55102be945ae
diashelter <helter.dias@gmail.com>
2024-06-13 12:18:20 -0300

    feat: pagination data of orders in table

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ type ISearchParams = {
     search?: string;
     status?: string;
     sort?: string;
+    page?: number;
   };
 };
 
@@ -25,17 +26,20 @@ export default async function Component({ searchParams }: ISearchParams) {
   const searchString = searchParams?.search;
   const statuString = searchParams?.status;
   const sortString = searchParams?.sort;
+  const page = searchParams?.page;
 
   const response = await axios.get(urlBase, {
     params: {
       search: searchString,
       status: statuString,
       sort: sortString,
+      page: page,
     },
   });
 
   const orders = response.data.data;
-  console.log(orders);
+  const links = response.data.meta.links;
+  const maxPage = response.data.meta.last_page;
 
   return (
     <main className="container px-1 py-10 md:p-10">
@@ -53,7 +57,7 @@ export default async function Component({ searchParams }: ISearchParams) {
         <CardContent>
           <OrdersTable orders={orders} />
           <div className="mt-8">
-            <Pagination />
+            <Pagination links={links} maxPage={maxPage}/>
           </div>
         </CardContent>
       </Card>
--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Pagination as PaginationComponent,
   PaginationContent,
@@ -6,38 +8,91 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from '@/components/ui/pagination';
+} from "@/components/ui/pagination";
+import { link } from "fs";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function Pagination() {
+type Link = {
+  url: string;
+  label: string;
+  active: boolean;
+};
+
+type LinkProps = {
+  links: Link[];
+  maxPage: number;
+};
+
+export default function Pagination({ links, maxPage }: LinkProps) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  function handleClickItemPagination(pageNumber: number) {
+    const params = new URLSearchParams(searchParams);
+
+    if (pageNumber > 1 || (pageNumber !== 1 && pageNumber < maxPage)) {
+      params.set("page", `${pageNumber}`);
+    } else {
+      params.delete("page");
+    }
+    replace(`${pathName}?${params.toString()}`, { scroll: false });
+  }
+
   return (
     <PaginationComponent>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious />
+        <PaginationItem
+          className={`${links[0].url ? "cursor-pointer" : "hidden"}`}
+        >
+          <PaginationPrevious
+            onClick={() =>
+              handleClickItemPagination(
+                Number(searchParams.get("page") || 1) - 1
+              )
+            }
+          />
         </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink isActive={true}>1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>2</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>8</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>9</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>10</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext />
+
+        {links.map((link, index) => {
+          if (
+            link.label.includes("Anterior") ||
+            link.label.includes("Próximo")
+          ) {
+            return null;
+          }
+
+          if (link.label === "...") {
+            return (
+              <PaginationItem key={index} className="hidden md:inline-flex">
+                <PaginationEllipsis />
+              </PaginationItem>
+            );
+          }
+
+          return (
+            <PaginationItem key={index} className="cursor-pointer">
+              <PaginationLink
+                onClick={() => handleClickItemPagination(Number(link.label))}
+                isActive={link.active}
+                dangerouslySetInnerHTML={{ __html: link.label }}
+              ></PaginationLink>
+            </PaginationItem>
+          );
+        })}
+
+        <PaginationItem
+          className={`${
+            links[links.length - 1].url ? "cursor-pointer" : "hidden"
+          }`}
+        >
+          <PaginationNext
+            onClick={() =>
+              handleClickItemPagination(
+                Number(searchParams.get("page") || 1) + 1
+              )
+            }
+          />
         </PaginationItem>
       </PaginationContent>
     </PaginationComponent>
